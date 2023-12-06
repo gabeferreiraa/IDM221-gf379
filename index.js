946ce17a9e4097923bae8f89ee1aabd2aba5e481
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2023-12-06 15:02:30 -0500

    Cmtd out resume, added box changing for index

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const hamburger = document.querySelector(".hamburger");
 const headermenu = document.querySelector(".header__menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
   headermenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
